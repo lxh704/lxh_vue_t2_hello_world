01e581584604ebe1729934553831252b708e2cbd
01434884 <liuxionghui@sf-express.com>
2023-12-29 08:59:14 +0800

    lxh first commit

---
+++ src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 @click="showName">{{name}}</h1>
+    <h2>{{address}}</h2>
+  </div>
+</template>
+
+<script>
+import {my_mix_one} from "@/myMixin";
+export default {
+  name: "School"
+  ,mixins:[my_mix_one]
+  ,data(){
+    return {
+      name:"厦门测试学校 first commit"
+      ,address:"厦门软件园"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
