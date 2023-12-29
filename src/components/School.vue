cabfd89db4ce226920e6ba5dcedf26c8a1906fad
01434884 <liuxionghui@sf-express.com>
2023-12-29 09:23:33 +0800

    master commit 4

--- src/components/School.vue
+++ src/components/School.vue
@@ -12,6 +12,7 @@ export default {
   ,mixins:[my_mix_one]
   ,data(){
     return {
+      // master 中 11111111
       // 这个是在hot fix中写的注释
       name:"厦门测试学校 real third commit"
       ,address:"厦门软件园"
